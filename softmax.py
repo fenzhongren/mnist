3324a02dca0826b07660c9803f37a0793badd378
fenzhongren <fenzhongren@163.com>
2018-12-21 16:29:47 +0800

    12-21 afternoon

--- softmax.py
+++ softmax.py
@@ -4,11 +4,20 @@ from mnist import testing_image_datasets
 from mnist import testing_label_datasets
 
 import numpy as np
+import csv
 
 num_classes = 10
 
 np.seterr(all='raise')
 #np.seterr(divide='raise')
+
+def save_matrix(ma):
+    with open('save.csv', 'w', newline = '') as f:
+        writer = csv.writer(f)
+        size = len(ma)
+        for i in range(size):
+            writer.writerow(ma[i])
+
 #images is a num_examples*num_features matrix
 #labels is a 1*num_examples matrix
 def get_data_from_datasets(image_datasets, label_datasets):
@@ -60,6 +69,32 @@ def calculate_gradient(images, labels, hypo):
     dtheta = -np.dot(images.T, adjust_hypoT)
     return dtheta
 
+#theta is num_features*num_classes matrix
+def test_gradient(images, labels):
+    num_features = len(images[0])
+    np.random.seed(0)
+    theta = np.random.randn(num_features, num_classes)
+    
+    gtheta = np.zeros(theta.shape)
+    for n in range(num_features):
+        for k in range(num_classes):
+            epsilon = np.zeros(theta.shape)
+            epsilon[n][k] = 0.0001
+            theta_plus = theta + epsilon
+            theta_minus = theta - epsilon
+            hypo_plus = calculate_hypo(images, theta_plus)
+            hypo_minus = calculate_hypo(images, theta_minus)
+            loss_plus = calculate_loss(images, labels, hypo_plus)
+            loss_minus = calculate_loss(images, labels, hypo_minus)
+            gtheta[n][k] = (loss_plus - loss_minus)/0.0002
+
+    hypo = calculate_hypo(images, theta)
+    dtheta = calculate_gradient(images, labels, hypo)
+
+    gtheta = np.around(gtheta, 4)
+    dtheta = np.around(dtheta, 4)
+    save_matrix((dtheta == gtheta))
+    
 
 def build_model(images, labels, num_pass=10000, step=0.01, print_loss=False):
     num_features = len(images[0])
@@ -79,4 +114,5 @@ def build_model(images, labels, num_pass=10000, step=0.01, print_loss=False):
 
     return theta
 
-theta = build_model(training_images, training_labels, step=0.2, print_loss=True)
+#theta = build_model(training_images, training_labels, step=0.21, print_loss=True)
+test_gradient(training_images, training_labels)
